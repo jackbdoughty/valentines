33c53c889c115344622ccc559fcb58209bf2f623
Jack Doughty <56323305+jackbdoughty@users.noreply.github.com>
2026-01-30 00:03:57 +0000

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function notFound() {
         <div className="container">
             <section>
                 <p>Page Not Found</p>
-                <Image src="/sadge.gif" alt="cute cat crying gif" width={340} height={300} />
+                <Image src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW9jMDI3bmdkZWpkYzJlZTc2dXRnZ2FqNzZrbHFqYXR2a3E2bWxtNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dbupKbo1Jg8mIIsia6/giphy.gif" alt="cute cat crying gif" width={340} height={300} />
             </section>
         </div>
     )
